1cd674f797b0a65b3fe8b29b97e37ce5fcedf9a1
Gabriel Taveira <gabrielstaveira@gmail.com>
2024-05-22 16:45:53 -0300

    add scheme

--- examples/kitchen-sink/app.config.ts
+++ examples/kitchen-sink/app.config.ts
@@ -4,6 +4,7 @@ export default {
   name: "react-native-magic-modal-example",
   displayName: "MagicModal Example",
   slug: "react-native-magic-modal-example",
+  scheme: "magic-modal",
   description: "Example app for react-native-magic-modal",
   privacy: "public",
   version: "1.0.0",
