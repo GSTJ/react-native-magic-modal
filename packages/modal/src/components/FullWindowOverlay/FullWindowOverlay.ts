8f0cb7291e31281fe058f7e6f68ce2d29adfe02c
Gabriel Taveira <gabrielstaveira@gmail.com>
2024-10-15 10:21:45 -0300

    fix: cjs errors while importing

--- packages/modal/src/components/FullWindowOverlay/FullWindowOverlay.ts
+++ packages/modal/src/components/FullWindowOverlay/FullWindowOverlay.ts
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Fragment } from "react";
 import { Platform } from "react-native";
+import { FullWindowOverlay as RNScreensFullWindowOverlay } from "react-native-screens";
 
 /** Don't use .ios file extension as bunchee can't bundle it properly */
 export const FullWindowOverlay =
-  Platform.OS === "ios"
-    ? require("react-native-screens").FullWindowOverlay
-    : Fragment;
+  Platform.OS === "ios" ? RNScreensFullWindowOverlay : Fragment;
