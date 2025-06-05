fe1e83a6da9c678dc8a677f6b93abbf0d597ca04
Gabriel Taveira <gabrielstaveira@gmail.com>
2025-06-05 20:03:37 -0300

    fix

--- packages/modal/eslint.config.js
+++ packages/modal/eslint.config.js
@@ -1,4 +1,10 @@
-import baseConfig from "@magic/eslint-config/react-native";
+import { join } from "path";
+import { includeIgnoreFile } from "@eslint/compat";
+
+import reactNativeConfig from "@magic/eslint-config/react-native";
 
 /** @type {import('typescript-eslint').Config} */
-export default [{ ignores: [] }, ...baseConfig];
+export default [
+  includeIgnoreFile(join(import.meta.dirname, "../../.gitignore")),
+  ...reactNativeConfig,
+];
