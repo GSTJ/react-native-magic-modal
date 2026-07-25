832e1eda3682f9f1f2f0162c0c6dada65fe77cdf
Gabriel Taveira <gabrielstaveira@gmail.com>
2026-07-25 17:02:04 -0300

    test: bring jest back to life and run it in CI
    Fixes #80

--- packages/modal/jest.config.js
+++ packages/modal/jest.config.js
@@ -1,5 +1,8 @@
 export default {
   preset: "jest-expo",
+  // Worklets ships a resolver that skips its `.native` entrypoints, which
+  // otherwise throw "Native part of Worklets doesn't seem to be initialized".
+  resolver: "react-native-worklets/jest/resolver.js",
   testResultsProcessor: "jest-junit",
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|react-native-gesture-handler|react-native-reanimated|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
